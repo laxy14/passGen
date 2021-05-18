868ccab500e7ad50f5679c28f57ce02da71a8879
Joshua Foster <83901337+laxy14@users.noreply.github.com>
2021-05-18 12:43:10 +1000

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -1,38 +1,50 @@
 import random
 import string
 
+password = ''
 
 def get_pass(length=12, uppercase=True, lowercase=True, numbers=True, punctuation=True):
-    char_set = ''
-    if uppercase:
-        char_set += string.ascii_uppercase
-    if lowercase:
-        char_set += string.ascii_lowercase
-    if numbers:
-        char_set += string.digits
-    if punctuation:
-        char_set += string.punctuation
+    try:
+        global password
+        char_set = ''
+        if uppercase:
+            char_set += string.ascii_uppercase
+        if lowercase:
+            char_set += string.ascii_lowercase
+        if numbers:
+            char_set += string.digits
+        if punctuation:
+            char_set += string.punctuation
+  
+        password = ''.join(random.choice(char_set) for i in range(length))     
+        return password
+
+    except IndexError:
+        print("You need to answer 'y' to atleast 1 input..")
+        password = ''
+        return main()
+       
         
-    return ''.join(random.choice(char_set) for i in range(length))
-
-
 def pass_length():
     while True:
         try:
             length = input("How man characters long would you like the password to be?: ").lower()
+            
             if length.isdigit() == False:
                 raise Exception("Please enter a number")
-                
             if length.isdigit():
-                    length = int(length)
-                    return length
-            else:
-                length = False
+                length = int(length)
+                if int(length) > 50:
+                    raise Exception("Enter a length < 50 chars..")
                 return length
-
+                
         except Exception as err:
             print(err)
             continue
+        except Exception as errLen:
+            print(errLen)
+            continue
+
 
 def pass_lower():
     while True:
@@ -49,6 +61,7 @@ def pass_lower():
         except Exception as err2:
             print(err2)
             continue
+
 
 def pass_upper():
     while True:
@@ -100,6 +113,21 @@ def pass_punctuation():
             print(err5)
             continue
 
+def write_to_file(): 
+    try:
+        global password
+        write = input("Would you like to write this to a .txt file?")
+        if write == 'y':
+            file1 = open(r"password.txt", "a+") #Opens file.. Creates in same DIR if doesnt exist.
+            file1.write(f"{password}\n")
+            file1.close() #Close file
+            print("Your password has been writen to file...")
+        else:
+            print(f"your password is: {password}")
+            input("Press ENTER to exit...")
+            quit()
+    except:
+        pass
 
 def main():
     get_length = pass_length()
@@ -107,8 +135,11 @@ def main():
     get_lower = pass_lower()
     get_numbers = pass_numbers()
     get_punct = pass_punctuation()
-    get_password = get_pass(length=get_length, uppercase=get_upper, lowercase=get_lower, numbers=get_numbers, punctuation=get_punct)
-    print(get_password)
-
+    get_password = get_pass(length=get_length, uppercase=get_upper,\
+        lowercase=get_lower, numbers=get_numbers, punctuation=get_punct)
+    return get_password
+ 
+    
 if __name__ == "__main__":
     main()
+    write_to_file()
